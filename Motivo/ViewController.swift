8b940b403816181052789a6d44d3d5bff36826f3
centerkeycooper <cooper.wilk@tagmagnet.org>
2025-03-03 19:28:51 -0600

    Commit Test with file structure

---
+++ Motivo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Motivo
+//
+//  Created by Aaron Lee on 3/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
