fda88fdb74bac9de60085acfee5ea0268dc7201a
arisyia <arisyia.wong@gmail.com>
2025-03-29 23:23:47 -0500

    Added Label Icon button type & updated Group Entry UI

---
+++ Motivo/Views/General/LabelIconButton.swift
@@ -0,0 +1,92 @@
+//
+//  GroupEntryButton.swift
+//  Motivo
+//
+//  Created by Arisyia Wong on 3/29/25.
+//
+
+import UIKit
+
+let buttonLength:CGFloat = 150
+
+class LabelIconButton: UIButton {
+    private var image = UIImage(systemName: "exclamationmark.triangle")
+    private var title = NormalLabel(textLabel: "Not Defined")
+    private var mainStackView: UIStackView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        mainStackView = UIStackView()
+        setupLabelIconButtonUI()
+    }
+    
+    init(image: UIImage) {
+        super.init(frame: .zero)
+        self.image = image
+        setupLabelIconButtonUI()
+    }
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        self.title = NormalLabel(textLabel: title)
+        setupLabelIconButtonUI()
+    }
+    
+    init(image: UIImage, title: String) {
+        super.init(frame: .zero)
+        self.image = image
+        self.title = NormalLabel(textLabel: title)
+        setupLabelIconButtonUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // function runs after layout is calculated
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//        
+//        self.layer.cornerRadius = 0.1 * groupEntryButtonLength
+//        self.layer.masksToBounds = true
+//    }
+    
+    private func setupLabelIconButtonUI() {
+        backgroundColor = .systemBackground
+        layer.borderColor = colorMainAccent.cgColor
+        layer.borderWidth = 2
+        layer.cornerRadius = 0.1 * buttonLength
+        
+        let imageView = UIImageView(image: image)
+        imageView.tintColor = colorMainAccent
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        
+        title.changeFontSize(fontSize: 20)
+        
+        mainStackView = UIStackView(arrangedSubviews: [imageView, title])
+        mainStackView.axis = .vertical
+        mainStackView.distribution = .fill
+        mainStackView.layoutMargins = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        mainStackView.isLayoutMarginsRelativeArrangement = true
+        
+        imageView.isUserInteractionEnabled = false
+        title.isUserInteractionEnabled = false
+        mainStackView.isUserInteractionEnabled = false
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        title.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(mainStackView)
+        sendSubviewToBack(mainStackView)
+        
+        NSLayoutConstraint.activate([
+            self.widthAnchor.constraint(equalToConstant: buttonLength),
+            self.heightAnchor.constraint(equalToConstant: buttonLength),
+            mainStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            mainStackView.widthAnchor.constraint(equalToConstant: buttonLength),
+            mainStackView.heightAnchor.constraint(equalToConstant: buttonLength)
+        ])
+    }
+}
--- Motivo/Views/GroupEntry/GroupEntryView.swift
+++ Motivo/Views/GroupEntry/GroupEntryView.swift
@@ -14,11 +14,11 @@ protocol GroupEntryViewDelegate:GroupEntryViewController {
 }
 
 class GroupEntryView: UIView {
-    let titleLabel = UILabel()
-    let subtitleLabel = UILabel()
-    let createNewGroupButton = UIButton()
-    let joinRandomGroupButton = UIButton()
-    let joinInviteGroupButton = UIButton()
+    let titleLabel = BoldTitleLabel(textLabel: "Match with a Group")
+    let subtitleLabel = SubtitleLabel(textLabel: "Join groups with these interests")
+    let createNewGroupButton = LabelIconButton(image: UIImage(systemName: "person.2.fill")!, title: "Create New")
+    let joinRandomGroupButton = LabelIconButton(image: UIImage(systemName: "shuffle")!, title: "Join Random")
+    let joinInviteGroupButton = LabelIconButton(image: UIImage(systemName: "person.fill.badge.plus")!, title: "Join Group")
     
     var delegate:GroupEntryViewDelegate?
     
@@ -32,58 +32,78 @@ class GroupEntryView: UIView {
      }
     
     private func setupUI() {
-        titleLabel.textAlignment = .center
-        titleLabel.text = "Match with a Group"
-        titleLabel.textColor = .systemBlue
-        subtitleLabel.textAlignment = .center
-        subtitleLabel.text = "Join groups with these interests"
         
-        createNewGroupButton.layer.borderColor = UIColor.black.cgColor
-        createNewGroupButton.layer.borderWidth = 2
-        createNewGroupButton.layer.cornerRadius = 8.0
-        createNewGroupButton.setTitleColor(.systemBlue, for: .normal)
-        createNewGroupButton.setTitle("Create New", for: .normal)
-        
-        joinRandomGroupButton.layer.borderColor = UIColor.black.cgColor
-        joinRandomGroupButton.layer.borderWidth = 2
-        joinRandomGroupButton.layer.cornerRadius = 8.0
-        joinRandomGroupButton.setTitleColor(.systemBlue, for: .normal)
-        joinRandomGroupButton.setTitle("Join Random", for: .normal)
-        
-        joinInviteGroupButton.layer.borderColor = UIColor.black.cgColor
-        joinInviteGroupButton.layer.borderWidth = 2
-        joinInviteGroupButton.layer.cornerRadius = 8.0
-        joinInviteGroupButton.setTitleColor(.systemBlue, for: .normal)
-        joinInviteGroupButton.setTitle("Join Group", for: .normal)
+        subtitleLabel.changeFontSize(fontSize: 20)
         
         createNewGroupButton.addTarget(self, action: #selector(touchedCreateGroupButton), for: .touchUpInside)
         joinRandomGroupButton.addTarget(self, action: #selector(touchedJoinRandomGroupButton), for: .touchUpInside)
         joinInviteGroupButton.addTarget(self, action: #selector(touchedJoinInviteGroupButton), for: .touchUpInside)
         
-        let stackView = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel, createNewGroupButton, joinRandomGroupButton, joinInviteGroupButton])
-        stackView.axis = .vertical
-        stackView.spacing = 15
-        addSubview(stackView)
+        let topButtonsStackView = UIStackView(arrangedSubviews: [createNewGroupButton, joinRandomGroupButton])
+        topButtonsStackView.axis = .horizontal
+        topButtonsStackView.spacing = 15
+        topButtonsStackView.distribution = .fillProportionally
+        topButtonsStackView.alignment = .center
         
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate(
-            [stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            // stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            stackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 50), // 50 pixels from top safe area
-            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
-            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40)
+        let bottomButtonsStackView = UIStackView(arrangedSubviews: [joinInviteGroupButton])
+        bottomButtonsStackView.axis = .horizontal
+        bottomButtonsStackView.distribution = .fill
+        bottomButtonsStackView.alignment = .center
+        
+        let mainStackView = UIStackView(arrangedSubviews: [topButtonsStackView, bottomButtonsStackView])
+        mainStackView.axis = .vertical
+        mainStackView.spacing = 15
+        mainStackView.distribution = .fill
+        mainStackView.alignment = .center
+        
+        addSubview(titleLabel)
+        addSubview(subtitleLabel)
+        addSubview(mainStackView)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        createNewGroupButton.translatesAutoresizingMaskIntoConstraints = false
+        joinRandomGroupButton.translatesAutoresizingMaskIntoConstraints = false
+        joinInviteGroupButton.translatesAutoresizingMaskIntoConstraints = false
+        topButtonsStackView.translatesAutoresizingMaskIntoConstraints = false
+        bottomButtonsStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            subtitleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            mainStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            mainStackView.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 50),
+            mainStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
+            mainStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+            topButtonsStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            bottomButtonsStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
         ])
     }
 
     @objc func touchedCreateGroupButton() {
+        UIView.animate(withDuration: 0.2) {
+            self.createNewGroupButton.backgroundColor = colorMainAccent.withAlphaComponent(0.2)
+            self.createNewGroupButton.backgroundColor = .systemBackground
+        }
         delegate?.didTouchCreateGroupButton()
     }
     
     @objc func touchedJoinRandomGroupButton() {
+        UIView.animate(withDuration: 0.2) {
+            self.joinRandomGroupButton.backgroundColor = colorMainAccent.withAlphaComponent(0.2)
+            self.joinRandomGroupButton.backgroundColor = .systemBackground
+        }
         delegate?.didTouchJoinRandomGroupButton()
     }
     
     @objc func touchedJoinInviteGroupButton() {
+        UIView.animate(withDuration: 0.2) {
+            self.joinInviteGroupButton.backgroundColor = colorMainAccent.withAlphaComponent(0.2)
+            self.joinInviteGroupButton.backgroundColor = .systemBackground
+        }
         delegate?.didTouchJoinInviteGroupButton()
     }
 }
