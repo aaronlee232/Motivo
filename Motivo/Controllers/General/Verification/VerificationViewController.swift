5455ed9ff265961c88e2da1608900b7dc93325e0
arisyia <arisyia.wong@gmail.com>
2025-04-20 00:33:12 -0500

    Some refactoring & created Verification View

--- Motivo/Controllers/General/Verification/VerificationViewController.swift
+++ Motivo/Controllers/General/Verification/VerificationViewController.swift
@@ -10,18 +10,18 @@ import Shuffle
 import Kingfisher
 
 struct CardData {
+    let user: UserModel // Which user the habit belongs to
     let image: UIImage
     let photoURL: String
     let habit: HabitModel
     let record: HabitRecord
-    
-    // TODO: ...Other info to display on card
 }
 
-class VerificationViewController: UIViewController {
+class VerificationViewController: UIViewController, VerificationViewDelegate {
+    
     // MARK: - UI Elements
     private var verificationView = VerificationView()
-    let cardStack = SwipeCardStack()
+//    let cardStack = SwipeCardStack()
     
     // MARK: - Properties
     let verificationManager = VerificationManager()
@@ -31,20 +31,22 @@ class VerificationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(cardStack)
+        view.backgroundColor = .systemBackground
+//        view.addSubview(cardStack)
         
-        cardStack.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            cardStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            cardStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            cardStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            cardStack.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor)
-//            cardStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
+//        cardStack.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            cardStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+//            cardStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+//            cardStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+//            cardStack.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor)
+////            cardStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+//        ])
         
-        cardStack.dataSource = self
-        cardStack.delegate = self
-        cardStack.reloadData()
+//        verificationView.cardStack.dataSource = self
+//        verificationView.cardStack.delegate = self
+        verificationView.cardStack.reloadData()
+        setupVerificationView()
     }
     
     func configureWith(user: UserModel, habitWithRecordsByUserUID: Dictionary<String, [HabitWithRecord]>, votedPhotoSet: Set<VotedPhoto>) {
@@ -63,56 +65,43 @@ class VerificationViewController: UIViewController {
                         }
                         
                         let image = try await ImageDownloader.fetchImage(from: url)
+                        guard let username: UserModel = try await FirestoreService.shared.fetchUser(forUserUID: entry.habit.userUID) else {
+                            print("Error: Failed to fetch user.")
+                            return
+                        }
                         cardData.append(
                             CardData(
+                                user: username,
                                 image: image,
                                 photoURL: url,
                                 habit: entry.habit,
                                 record: entry.record
                             )
                         )
+                        verificationView.verificationViewCardData = cardData
                     }
                 }
             } catch {
                 print("Failed to fetch image: \(error)")
             }
-            cardStack.reloadData()
+            verificationView.cardStack.reloadData()
         }
     }
     
-    func card(fromImage image: UIImage) -> SwipeCard {
-        let card = SwipeCard()
-        card.swipeDirections = [.left, .right]
-        card.content = UIImageView(image: image)
-        card.content?.contentMode = .scaleAspectFit
-
-        let leftOverlay = UIView()
-        leftOverlay.backgroundColor = .systemRed
-
-        let rightOverlay = UIView()
-        rightOverlay.backgroundColor = .systemGreen
-
-        card.setOverlays([.left: leftOverlay, .right: rightOverlay])
-
-        return card
+    private func setupVerificationView() {
+        view.addSubview(verificationView)
+        verificationView.delegate = self
+        
+        verificationView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            verificationView.topAnchor.constraint(equalTo: view.topAnchor),
+            verificationView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            verificationView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            verificationView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
-}
-
-extension VerificationViewController: SwipeCardStackDataSource, SwipeCardStackDelegate {
-    func cardStack(_ cardStack: SwipeCardStack, cardForIndexAt index: Int) -> SwipeCard {
-        // TODO: create a view that makes the card with description of the user and habit, maybe date
-        // Create a footer with arrow keys and x and check mark to do the same things that swiping does
-        // Another footer if want to skip someone and go to next connection
-        let verificationCard = VerificationCard()
-        return verificationCard.card(fromUser: "user", fromHabit: "habit", fromImage: cardData[index].image)
-//        return card(fromImage: cardData[index].image)
-    }
-
-    func numberOfCards(in cardStack: SwipeCardStack) -> Int {
-        return cardData.count
-    }
-
-    func cardStack(_ cardStack: SwipeCardStack, didSwipeCardAt index: Int, with direction: SwipeDirection) {
+    
+    func didSwipeCardAt(index: Int, direction: Shuffle.SwipeDirection) {
         Task {
             do {
                 // Retrieve logged in user's auth instance
@@ -157,11 +146,88 @@ extension VerificationViewController: SwipeCardStackDataSource, SwipeCardStackDe
         }
     }
     
-    func cardStack(_ cardStack: SwipeCardStack, didUndoCardAt index: Int, from direction: SwipeDirection) {
-        // Stub
-    }
-    
-    func didSwipeAllCards(_ cardStack: SwipeCardStack) {
-        // TODO: Let user know all photos for the current user have been reviewed
-    }
+//    func card(fromImage image: UIImage) -> SwipeCard {
+//        let card = SwipeCard()
+//        card.swipeDirections = [.left, .right]
+//        card.content = UIImageView(image: image)
+//        card.content?.contentMode = .scaleAspectFit
+//
+//        let leftOverlay = UIView()
+//        leftOverlay.backgroundColor = .systemRed
+//
+//        let rightOverlay = UIView()
+//        rightOverlay.backgroundColor = .systemGreen
+//
+//        card.setOverlays([.left: leftOverlay, .right: rightOverlay])
+//
+//        return card
+//    }
 }
+
+//extension VerificationViewController: SwipeCardStackDataSource, SwipeCardStackDelegate {
+//    func cardStack(_ cardStack: SwipeCardStack, cardForIndexAt index: Int) -> SwipeCard {
+//        // TODO: create a view that makes the card with description of the user and habit, maybe date
+//        // Create a footer with arrow keys and x and check mark to do the same things that swiping does
+//        // Another footer if want to skip someone and go to next connection
+//        let verificationCard = VerificationCard()
+//        return verificationCard.card(fromUser: "user", fromHabit: "habit", fromImage: cardData[index].image)
+////        return card(fromImage: cardData[index].image)
+//    }
+//
+//    func numberOfCards(in cardStack: SwipeCardStack) -> Int {
+//        return cardData.count
+//    }
+
+//    func cardStack(_ cardStack: SwipeCardStack, didSwipeCardAt index: Int, with direction: SwipeDirection) {
+//        Task {
+//            do {
+//                // Retrieve logged in user's auth instance
+//                guard let userAuthInstance = AuthManager.shared.getCurrentUserAuthInstance() else {
+//                    print("Error: No authenticated user.")
+//                    return
+//                }
+//                
+//                switch direction {
+//                case .left:
+//                    // Create reject vote for userID and photoURL at index
+//                    let vote = VoteModel(
+//                        voterUID: userAuthInstance.uid,
+//                        habitRecordID: cardData[index].record.id,
+//                        photoURL: cardData[index].photoURL,
+//                        voteType: VoteType.reject,
+//                        timestamp: ISO8601DateFormatter().string(from: Date())
+//                    )
+//                    try verificationManager.addVote(vote)
+//                case .right:
+//                    // Create accept vote for userID and photoURL at index
+//                    let vote = VoteModel(
+//                        voterUID: userAuthInstance.uid,
+//                        habitRecordID: cardData[index].record.id,
+//                        photoURL: cardData[index].photoURL,
+//                        voteType: VoteType.accept,
+//                        timestamp: ISO8601DateFormatter().string(from: Date())
+//                    )
+//                    try verificationManager.addVote(vote)
+//                    
+//                    // Move photoURL from unverified list to verified list in active habit record
+//                    try await verificationManager.approvePhoto(
+//                        withPhotoURL: cardData[index].photoURL,
+//                        forHabitRecordID: cardData[index].record.id
+//                    )
+//                default:
+//                    break
+//                }
+//            } catch {
+//                AlertUtils.shared.showAlert(self, title: "Something went wrong", message: "Unable to verify photo")
+//            }
+//        }
+//    }
+    
+//    func cardStack(_ cardStack: SwipeCardStack, didUndoCardAt index: Int, from direction: SwipeDirection) {
+//        // Stub
+//    }
+//    
+//    func didSwipeAllCards(_ cardStack: SwipeCardStack) {
+//        // TODO: Let user know all photos for the current user have been reviewed
+//    }
+//}
--- Motivo/Views/Verification/VerificationView.swift
+++ Motivo/Views/Verification/VerificationView.swift
@@ -6,13 +6,30 @@
 //
 
 import UIKit
+import Shuffle
 
-class VerificationView: UIView {
+protocol VerificationViewDelegate:VerificationViewController {
+    func didSwipeCardAt(index: Int, direction: SwipeDirection)
+}
+
+class VerificationView: UIView, SwipeCardStackDelegate, SwipeCardStackDataSource {
     
     private let titleLabel = BoldTitleLabel(textLabel: "Verify")
+    let cardStack = SwipeCardStack()
+    var verificationViewCardData:[CardData] = []
+    
+    // buttons stack - includes next image toggles, x and check buttons
+    private var buttonsStackView:UIStackView!
+    
+    // next connection view - shows next connection with button or doesn't show if there are no more photos to verify
+    private var nextConnectionStackView:UIStackView!
+    
+    var delegate:VerificationViewDelegate?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        cardStack.delegate = self
+        cardStack.dataSource = self
         setupUI()
     }
     
@@ -24,14 +41,50 @@ class VerificationView: UIView {
         titleLabel.textAlignment = .center
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        cardStack.translatesAutoresizingMaskIntoConstraints = false
         
         addSubview(titleLabel)
+        addSubview(cardStack)
         
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
             titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 60),
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
             titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            cardStack.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            cardStack.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            cardStack.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            cardStack.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor)
         ])
+    }
+    
+    @objc func handleDidSwipeCardAt(index: Int, direction: SwipeDirection) {
+        delegate?.didSwipeCardAt(index: index, direction: direction)
+    }
+    
+    func cardStack(_ cardStack: SwipeCardStack, cardForIndexAt index: Int) -> SwipeCard {
+        // TODO: create a view that makes the card with description of the user and habit, maybe date
+        // Create a footer with arrow keys and x and check mark to do the same things that swiping does
+        // Another footer if want to skip someone and go to next connection
+        let verificationCard = VerificationCard()
+        return verificationCard.card(fromUser: verificationViewCardData[index].user.username, fromHabit: verificationViewCardData[index].habit.name, fromImage: verificationViewCardData[index].image)
+//        return card(fromImage: cardData[index].image)
+    }
+    
+    func numberOfCards(in cardStack: SwipeCardStack) -> Int {
+        return verificationViewCardData.count
+    }
+    
+    func cardStack(_ cardStack: SwipeCardStack, didSwipeCardAt index: Int, with direction: SwipeDirection) {
+        handleDidSwipeCardAt(index: index, direction: direction)
+    }
+    
+    func cardStack(_ cardStack: SwipeCardStack, didUndoCardAt index: Int, from direction: SwipeDirection) {
+        // Stub
+    }
+    
+    func didSwipeAllCards(_ cardStack: SwipeCardStack) {
+        // TODO: Let user know all photos for the current user have been reviewed
     }
 }
